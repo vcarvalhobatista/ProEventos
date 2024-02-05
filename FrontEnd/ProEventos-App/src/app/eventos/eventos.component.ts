7f2f4edd09882bd05331fc61ab6d4c6fab2c1ab8
Vinícius Carvalho Batista <vinicius_carva@hotmail.com>
2024-02-05 10:22:01 -0300

    real time filter included

--- FrontEnd/ProEventos-App/src/app/eventos/eventos.component.ts
+++ FrontEnd/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,13 +9,38 @@ import { catchError, map } from 'rxjs/operators';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-  constructor(private http: HttpClient) {}
+
+  
   isCollapsed = true;
   widthImg = 150;
   marginImg = 2;
-  filtroLista = "";
+  private _filtroLista = "";
   public eventos: any = [];
-
+  public eventosFiltrados : any = [];
+  
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+  
+  public set filtroLista(value : string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this._filtroLista) : this.eventos
+  }
+  
+  filtrarEventos(filtrarPor : string) : any
+  {
+    filtrarPor = filtrarPor.toLowerCase();
+    
+    return this.eventos.filter(
+      (evento : any) => evento.tema.toLowerCase().indexOf(filtrarPor) !== -1 || 
+      evento.local.toLowerCase().indexOf(filtrarPor) !== -1
+      // ,
+      // console.log(filtrarPor)
+      )
+    }
+    
+  constructor(private http: HttpClient) {}
+  
   ngOnInit(): void {
     this.getEventos();
   }
@@ -27,7 +52,10 @@ export class EventosComponent implements OnInit {
   public getEventos(): void {
     this.http.get<any[]>('https://localhost:5001/api/Eventos')
       .pipe(
-        map(response => this.eventos = response),
+        map(response => {
+          this.eventos = response;
+          this.eventosFiltrados = this.eventos;
+        }),
         catchError(error => {
           console.error(error);
           return of([]); // ou qualquer valor padrão desejado
